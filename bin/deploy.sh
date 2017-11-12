cc00d7e2a88b472fac71b34c3f673e2a574acc8f
Dan Murphy <murphydbuffalo@gmail.com>
2017-11-11 19:08:48 -0700

    Simplify deploy script.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,14 +5,6 @@ set -e
 
 ./bin/build.sh
 
-# Set `Content-Encoding` to `gzip` for HTML assets.
-aws s3 sync ./build s3://smile-o-meter.rocks --acl public-read --exclude "*" --include "*.html"  --content-encoding "gzip"
-
-# Set `Content-Encoding` to `gzip` and set `Cache-Control` to `maxage=31536000` for CSS & JS manifest files.
-aws s3 sync ./build s3://smile-o-meter.rocks --acl public-read --exclude "*" --include "*.css" --include "*.js" --content-encoding "gzip" --cache-control "max-age=31536000"
-
-# Set `Cache-Control` to `maxage=31536000` for images.
-aws s3 sync ./build s3://smile-o-meter.rocks --acl public-read --exclude "*" --include "images/*" --cache-control "max-age=31536000"
-
-# Upload remaining files (favicons) as-is.
-aws s3 sync ./build s3://smile-o-meter.rocks --acl public-read --exclude "*.html" --exclude "*.css" --exclude "*.js" --exclude "images/*"
+# Sync local build director with S3 Bucket
+aws s3 sync ./build s3://smile-o-meter.rocks --acl public-read --exclude "*.css" --exclude "*.js" --exclude "images/*"
+aws s3 sync ./build s3://smile-o-meter.rocks --acl public-read --exclude "*" --include "*.css" --include "*.js" --include "images/*" --cache-control "max-age=31536000"
