a6142387593fb8930f3ff131f7ce86e1b183d913
Dan Murphy <murphydbuffalo@gmail.com>
2017-12-16 14:44:52 -0700

    Xavier/He initialization of weights and biases.

---
+++ neural_net/initialize.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class Initialize:
+    xavier_numerator = 1
+    he_numerator     = 2
+
+    def __init__(self, network_shape, algorithm = 'xavier'):
+        self.network_shape = network_shape
+        self.algorithm     = algorithm
+
+    def weights_and_biases(self):
+        weights = []
+        biases  = []
+
+        for i in range(0, len(self.network_shape) - 1):
+            num_inputs  = self.network_shape[i]
+            num_outputs = self.network_shape[i + 1]
+
+            weights.append(self.__initialize_weights(num_inputs, num_outputs))
+            biases.append(self.__initialize_biases(num_outputs))
+
+        return [weights, biases]
+
+    def __initialize_weights(self, num_inputs, num_outputs):
+        if self.algorithm == 'xavier':
+            numerator = self.xavier_numerator
+        else:
+            numerator = self.he_numerator
+
+        return np.random.randn(num_outputs, num_inputs) * np.sqrt(numerator / num_inputs)
+
+    def __initialize_biases(self, num_outputs):
+       return np.zeros((num_outputs, 1))
