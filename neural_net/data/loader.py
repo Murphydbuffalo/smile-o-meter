5746733aa29042c127f9022fa5512d2509d97d5b
Dan Murphy <murphydbuffalo@gmail.com>
2017-12-25 18:23:35 -0500

    Remove labels for 0 variance examples.

--- neural_net/data/loader.py
+++ neural_net/data/loader.py
@@ -16,21 +16,12 @@ class Loader:
         self.Xtest          = None
         self.Ytest          = None
 
-    # Normalizes input data to have mean 0 and variance 1
-    def __norm(self, matrix):
-        means                                 = np.mean(matrix, 0)
-        mean_zero_data                        = matrix - means
-        standard_deviations                   = np.std(mean_zero_data, 0)
-        normalized                            = mean_zero_data / standard_deviations
-        columns_with_zero_standard_devivation = np.where(standard_deviations == 0)[0]
-
-        return np.delete(normalized, columns_with_zero_standard_devivation, axis = 1)
-
     def normalize(self):
         self.Xtrain_norm = self.__norm(self.Xtrain)
         self.Xdev_norm   = self.__norm(self.Xdev)
         self.Xtest_norm  = self.__norm(self.Xtest)
 
+    # Need to filter out labels for bad data examples
     def load(self):
         for data_source in self.data_sources:
             data_source.load_data(self.print_progress)
@@ -53,3 +44,32 @@ class Loader:
 
                 self.Xtest = np.column_stack((self.Xtest, data_source.Xtest))
                 self.Ytest = np.column_stack((self.Ytest, data_source.Ytest))
+
+        Xtrain_no_bad_data, Ytrain_no_bad_data = self.__remove_zero_standard_deviation_examples(self.Xtrain, self.Ytrain)
+        self.Xtrain = Xtrain_no_bad_data
+        self.Ytrain = Ytrain_no_bad_data
+
+        Xdev_no_bad_data, Ydev_no_bad_data = self.__remove_zero_standard_deviation_examples(self.Xdev, self.Ydev)
+        self.Xdev = Xdev_no_bad_data
+        self.Ydev = Ydev_no_bad_data
+
+        Xtest_no_bad_data, Ytest_no_bad_data = self.__remove_zero_standard_deviation_examples(self.Xtest, self.Ytest)
+        self.Xtest = Xtest_no_bad_data
+        self.Ytest = Ytest_no_bad_data
+
+    # Normalizes input data to have mean 0 and variance 1
+    def __norm(self, matrix):
+        means                                 = np.mean(matrix, 0)
+        mean_zero_data                        = matrix - means
+        standard_deviations                   = np.std(mean_zero_data, 0)
+
+        return mean_zero_data / standard_deviations
+
+    def __remove_zero_standard_deviation_examples(self, matrix, labels):
+        standard_deviations                   = np.std(matrix, 0)
+        columns_with_zero_standard_devivation = np.where(standard_deviations == 0)[0]
+
+        return [
+            np.delete(matrix, columns_with_zero_standard_devivation, axis = 1),
+            np.delete(labels, columns_with_zero_standard_devivation, axis = 1)
+        ]
