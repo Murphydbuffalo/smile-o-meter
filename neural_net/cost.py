35af4f080960573581edb8d0757211f4ca6c99a8
Dan Murphy <murphydbuffalo@gmail.com>
2017-12-28 14:05:08 -0500

    Use `np.choose` instead of for-loop in cost function.

--- neural_net/cost.py
+++ neural_net/cost.py
@@ -2,24 +2,10 @@ import numpy as np
 
 class Cost:
     def __init__(self, predictions, labels):
-        self.predictions = predictions
-        self.labels      = labels[0]
+        # take only the softmax output for the class of the label
+        self.predictions = np.choose(labels, predictions)
+        self.labels      = labels
+        self.m           = float(labels.shape[1])
 
     def cross_entropy_loss(self):
-        m     = len(self.labels)
-        total = 0
-
-        for i in range(0, m):
-            label       = self.labels[i]
-            confidence  = self.predictions[label][i]
-            total      += -np.log(confidence)
-
-        return total / m
-
-    # TODO: See if one of these performs better:
-    # totals = [-np.log(self.predictions[self.labels[i]][i]) for i in range(m)]
-    # return np.sum(totals) / m
-    #
-    # Haven't tested this out...
-    # a = np.zip(labels, predictions)
-    # np.apply_along_axis(self.__loss_for_example, 0, a)
+        return np.sum(-np.log(self.predictions)) / self.m
