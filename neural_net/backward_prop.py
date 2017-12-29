7dddc0a0211cf89660ccf4e18cd76eab65db86b2
Dan Murphy <murphydbuffalo@gmail.com>
2017-12-29 12:32:11 -0500

    Comment with the desired shapes of the gradient matrixes.

--- neural_net/backward_prop.py
+++ neural_net/backward_prop.py
@@ -3,32 +3,27 @@ import numpy as np
 class BackwardProp:
     def __init__(self, Z, A, labels):
         self.Z              = Z      # Z (W * X + B) and A (activation(Z)) are lists with L (# of layers) elements:
-        self.A              = A      # [ input -> 2304 x m, hidden -> 5 x m, hidden -> 3 x m, output -> 3 x m ]
+        self.A              = A      # [ hidden = 5 x m, hidden = 3 x m, output = 1 x m ]
         self.labels         = labels # 1 x m
         self.softmax_output = A[-1]  # 1 x m ...only the predictions for the class identified by the label of each example.
 
     def run(self):
-        weight_gradients = []
-        bias_gradients   = []
+        # these should have the following shapes (a derivative for each weight/bias)
+        weight_gradients = [] # [5 x 2304, 3 x 5, 3 x 3]
+        bias_gradients   = [] # [5 x 1,    3 x 1, 3 x 1]
 
         for i in range(1, len(self.A)):
-            if(i == 1):
-                dw = self.__d_cost_d_w()
-                db = self.__d_cost_d_b()
+            if i == 1:
+                dw = self.__d_cost_d_softmax() * self.__d_softmax_d_z() * self.__d_z_d_w(-i)
+                db = self.__d_cost_d_softmax() * self.__d_softmax_d_z() * self.__d_z_d_b()
             else:
-                dw = weight_gradients[-1] * self.__d_relu_d_z(reverse_index) * self.__d_z_d_w(-i)
-                db = bias_gradients[-1]   * self.__d_relu_d_z(reverse_index) * self.__d_z_d_b()
+                dw = weight_gradients[-1] * self.__d_relu_d_z(-i) * self.__d_z_d_w(-i)
+                db = bias_gradients[-1]   * self.__d_relu_d_z(-i) * self.__d_z_d_b()
 
             weight_gradients.append(dw)
             bias_gradients.append(db)
 
         return [weight_gradients.reverse(), bias_gradients.reverse()]
-
-    def __d_cost_d_w(self):
-        return self.__d_cost_d_softmax() * self.__d_softmax_d_z() * self.__d_z_d_w(-1)
-
-    def __d_cost_d_b(self):
-        return self.__d_cost_d_softmax() * self.__d_softmax_d_z() * self.__d_z_d_b()
 
     def __d_cost_d_softmax(self):
         return -1 / self.softmax_output # 1 x m
@@ -40,14 +35,12 @@ class BackwardProp:
         # Z[-1] ->    1 x m
         # Z[-2] ->    3 x m
         # Z[-3] ->    5 x m
-        # Z[-4] -> 2304 x m
         return np.where(self.Z[layer] > 0, 1, 0)
 
     def __d_z_d_w(self, layer):
         # A[-1] ->    1 x m
         # A[-2] ->    3 x m
         # A[-3] ->    5 x m
-        # A[-4] -> 2304 x m
         return self.A[layer - 1]
 
     def __d_z_d_b(self):
--- neural_net/model.py
+++ neural_net/model.py
@@ -6,7 +6,7 @@ from forward_prop  import ForwardProp
 from cost          import Cost
 from backward_prop import BackwardProp
 
-d = Loader()
+d = Loader(True)
 d.load()
 d.normalize()
 
@@ -24,8 +24,11 @@ Z, A            = ForwardProp(weights, biases, d.Xtrain_norm).run()
 # predictions for the class corresponding to the label, y, for each example.
 # When using the network to make predictions we'll keep the predictions for all
 # classes and choose the one with the highest probability.
-predictions                      = np.choose(d.Ytrain, A[-1])
+output_layer_linear_activations  = np.choose(d.Ytrain, Z[-1])
+output_layer_softmax_activations = np.choose(d.Ytrain, A[-1])
+Ztrain                           = copy.copy(Z)
+Ztrain[-1]                       = output_layer_linear_activations
 Atrain                           = copy.copy(A)
-Atrain[-1]                       = predictions
+Atrain[-1]                       = output_layer_softmax_activations
 c                                = Cost(predictions, d.Ytrain).cross_entropy_loss()
-weight_gradients, bias_gradients = BackwardProp(Z, Atrain, d.Ytrain).run()
+weight_gradients, bias_gradients = BackwardProp(Ztrain, Atrain, d.Ytrain).run()
