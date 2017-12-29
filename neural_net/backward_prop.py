891678311f29a08a293c52602478e044a465a614
Dan Murphy <murphydbuffalo@gmail.com>
2017-12-29 13:37:57 -0500

    Output proper dimensions for gradient matrixes.

--- neural_net/backward_prop.py
+++ neural_net/backward_prop.py
@@ -3,7 +3,7 @@ import numpy as np
 class BackwardProp:
     def __init__(self, Z, A, labels):
         self.Z              = Z      # Z (W * X + B) and A (activation(Z)) are lists with L (# of layers) elements:
-        self.A              = A      # [ hidden = 5 x m, hidden = 3 x m, output = 1 x m ]
+        self.A              = A      # [ hidden = 5 x m, hidden = 3 x m, output = 3 x m ]
         self.labels         = labels # 1 x m
         self.softmax_output = A[-1]  # 1 x m ...only the predictions for the class identified by the label of each example.
 
@@ -14,31 +14,33 @@ class BackwardProp:
 
         for i in range(1, len(self.A)):
             if i == 1:
-                dw = self.__d_cost_d_softmax() * self.__d_softmax_d_z() * self.__d_z_d_w(-i)
-                db = self.__d_cost_d_softmax() * self.__d_softmax_d_z() * self.__d_z_d_b()
+                dw = np.dot(self.__d_cost_d_softmax() * self.__d_softmax_d_z(), self.__d_z_d_w(-i).T)
+                db = np.sum(self.__d_cost_d_softmax() * self.__d_softmax_d_z(), axis = 1, keepdims = True) * self.__d_z_d_b()
             else:
-                dw = weight_gradients[-1] * self.__d_relu_d_z(-i) * self.__d_z_d_w(-i)
-                db = bias_gradients[-1]   * self.__d_relu_d_z(-i) * self.__d_z_d_b()
+                dw = np.dot(weight_gradients[-1], np.dot(self.__d_z_d_w(-i), self.__d_relu_d_z(-i).T).T)
+                db = np.sum(self.__d_relu_d_z(-i), axis = 1, keepdims = True) * bias_gradients[-1] * self.__d_z_d_b()
 
+            print("dw.shape is", dw.shape)
+            print("db.shape is", db.shape)
             weight_gradients.append(dw)
             bias_gradients.append(db)
 
         return [weight_gradients.reverse(), bias_gradients.reverse()]
 
     def __d_cost_d_softmax(self):
-        return -1 / self.softmax_output # 1 x m
+        return -1 / self.softmax_output # 3 x m
 
     def __d_softmax_d_z(self):
-        return self.softmax_output * (1 - self.softmax_output) # 1 x m
+        return self.softmax_output * (1 - self.softmax_output) # 3 x m
 
     def __d_relu_d_z(self, layer):
-        # Z[-1] ->    1 x m
+        # Z[-1] ->    3 x m
         # Z[-2] ->    3 x m
         # Z[-3] ->    5 x m
         return np.where(self.Z[layer] > 0, 1, 0)
 
     def __d_z_d_w(self, layer):
-        # A[-1] ->    1 x m
+        # A[-1] ->    3 x m
         # A[-2] ->    3 x m
         # A[-3] ->    5 x m
         return self.A[layer - 1]
