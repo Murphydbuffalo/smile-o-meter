4055cce4e66a079ea0c9b3301e7a8f82a36d2ecc
Dan Murphy <murphydbuffalo@gmail.com>
2017-12-29 13:35:25 -0500

    Don't need to modify activation dimensions.
    When passing `Z` and `A` to backprop. We need gradients for all weights,
    not only the weights of the activations used to calculate the cost
    (because we use a softmax activation in the final layer, which outputs a
    vector with elements for each class, we only use a subset of the final
    activations in the cost function - those activations that are the predictions
    for the label corresponding to a particular example).

--- neural_net/model.py
+++ neural_net/model.py
@@ -6,7 +6,7 @@ from forward_prop  import ForwardProp
 from cost          import Cost
 from backward_prop import BackwardProp
 
-d = Loader(True)
+d = Loader()
 d.load()
 d.normalize()
 
@@ -20,15 +20,11 @@ network_architecture = [
 weights, biases = Initialize(network_architecture).weights_and_biases()
 Z, A            = ForwardProp(weights, biases, d.Xtrain_norm).run()
 
-# To calculate the cost and gradients of the network we take only the softmax
-# predictions for the class corresponding to the label, y, for each example.
+# To calculate the cost of the network we take only the softmax predictions for
+# the class corresponding to the label, y, for each example.
+#
 # When using the network to make predictions we'll keep the predictions for all
 # classes and choose the one with the highest probability.
-output_layer_linear_activations  = np.choose(d.Ytrain, Z[-1])
-output_layer_softmax_activations = np.choose(d.Ytrain, A[-1])
-Ztrain                           = copy.copy(Z)
-Ztrain[-1]                       = output_layer_linear_activations
-Atrain                           = copy.copy(A)
-Atrain[-1]                       = output_layer_softmax_activations
+predictions                      = np.choose(d.Ytrain, A[-1])
 c                                = Cost(predictions, d.Ytrain).cross_entropy_loss()
-weight_gradients, bias_gradients = BackwardProp(Ztrain, Atrain, d.Ytrain).run()
+weight_gradients, bias_gradients = BackwardProp(Z, A, d.Ytrain).run()
