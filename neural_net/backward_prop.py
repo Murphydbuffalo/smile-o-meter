506260c2a7cadecaa8c3d451ccdf9ccc290cee23
Dan Murphy <murphydbuffalo@gmail.com>
2017-12-29 11:40:06 -0500

    Comments denoting all expected matrix dimensions.
    And a cleaner `for` loop in `BackwardProp`.

--- neural_net/backward_prop.py
+++ neural_net/backward_prop.py
@@ -2,23 +2,25 @@ import numpy as np
 
 class BackwardProp:
     def __init__(self, Z, A, labels):
-        self.Z              = Z
-        self.A              = A
-        self.labels         = labels
-        self.softmax_output = np.choose(labels, A[-1])
+        self.Z              = Z      # Z (W * X + B) and A (activation(Z)) are lists with L (# of layers) elements:
+        self.A              = A      # [ input -> 2304 x m, hidden -> 5 x m, hidden -> 3 x m, output -> 3 x m ]
+        self.labels         = labels # 1 x m
+        self.softmax_output = A[-1]  # 1 x m ...only the predictions for the class identified by the label of each example.
 
     def run(self):
-        weight_gradients = [self.__d_cost_d_w()]
-        bias_gradients   = [self.__d_cost_d_b()]
-        reverse_index    = -2
+        weight_gradients = []
+        bias_gradients   = []
 
-        for i in range(len(self.A) - 1):
-            d_w = weight_gradients[-1] * self.__d_relu_d_z(reverse_index) * self.__d_z_d_w(reverse_index)
-            d_b = bias_gradients[-1]   * self.__d_relu_d_z(reverse_index) * self.__d_z_d_b()
+        for i in range(1, len(self.A)):
+            if(i == 1):
+                dw = self.__d_cost_d_w()
+                db = self.__d_cost_d_b()
+            else:
+                dw = weight_gradients[-1] * self.__d_relu_d_z(reverse_index) * self.__d_z_d_w(-i)
+                db = bias_gradients[-1]   * self.__d_relu_d_z(reverse_index) * self.__d_z_d_b()
 
-            weight_gradients.append(d_w)
-            bias_gradients.append(d_b)
-            reverse_index -= 1
+            weight_gradients.append(dw)
+            bias_gradients.append(db)
 
         return [weight_gradients.reverse(), bias_gradients.reverse()]
 
@@ -29,15 +31,23 @@ class BackwardProp:
         return self.__d_cost_d_softmax() * self.__d_softmax_d_z() * self.__d_z_d_b()
 
     def __d_cost_d_softmax(self):
-        return -1 / self.softmax_output
+        return -1 / self.softmax_output # 1 x m
 
     def __d_softmax_d_z(self):
-        return self.softmax_output * (1 - self.softmax_output)
+        return self.softmax_output * (1 - self.softmax_output) # 1 x m
 
     def __d_relu_d_z(self, layer):
+        # Z[-1] ->    1 x m
+        # Z[-2] ->    3 x m
+        # Z[-3] ->    5 x m
+        # Z[-4] -> 2304 x m
         return np.where(self.Z[layer] > 0, 1, 0)
 
     def __d_z_d_w(self, layer):
+        # A[-1] ->    1 x m
+        # A[-2] ->    3 x m
+        # A[-3] ->    5 x m
+        # A[-4] -> 2304 x m
         return self.A[layer - 1]
 
     def __d_z_d_b(self):
--- neural_net/model.py
+++ neural_net/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy  as copy
 from data.loader   import Loader
 from initialize    import Initialize
 from forward_prop  import ForwardProp
@@ -16,7 +17,15 @@ network_architecture = [
     3
 ]
 
-weights, biases                  = Initialize(network_architecture).weights_and_biases()
-predictions, Z, A                = ForwardProp(weights, biases, d.Xtrain_norm).run()
+weights, biases = Initialize(network_architecture).weights_and_biases()
+Z, A            = ForwardProp(weights, biases, d.Xtrain_norm).run()
+
+# To calculate the cost and gradients of the network we take only the softmax
+# predictions for the class corresponding to the label, y, for each example.
+# When using the network to make predictions we'll keep the predictions for all
+# classes and choose the one with the highest probability.
+predictions                      = np.choose(d.Ytrain, A[-1])
+Atrain                           = copy.copy(A)
+Atrain[-1]                       = predictions
 c                                = Cost(predictions, d.Ytrain).cross_entropy_loss()
-weight_gradients, bias_gradients = BackwardProp(Z, A, d.Ytrain).run()
+weight_gradients, bias_gradients = BackwardProp(Z, Atrain, d.Ytrain).run()
