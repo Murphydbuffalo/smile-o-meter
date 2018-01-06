fb09d7cb4dbdba37984ba2a15182f823813a77d5
Dan Murphy <murphydbuffalo@gmail.com>
2018-01-06 13:05:47 -0700

    The cost function should take both labels & predictions as inputs.
    Rather than `model.py` transforming the output of the network based on
    the labels, and passing that transformed result to the cost function.
    
    Making this change means that we can calculate the derivative of the
    cost function properly (aka by calculating it with respect to a
    multivariate function)..

--- neural_net/cost.py
+++ neural_net/cost.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class Cost:
     def __init__(self, predictions, labels):
-        self.predictions = predictions
-        self.labels      = labels
+        # To calculate the cost of the network we take only the softmax predictions
+        # for the class corresponding to the label of each example.
+        self.predictions = np.choose(labels, predictions)
         self.m           = float(labels.shape[1])
 
     def cross_entropy_loss(self):
--- neural_net/model.py
+++ neural_net/model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy  as copy
 from data.loader   import Loader
 from initialize    import Initialize
 from forward_prop  import ForwardProp
@@ -17,14 +16,7 @@ network_architecture = [
     3
 ]
 
-weights, biases = Initialize(network_architecture).weights_and_biases()
-Z, A            = ForwardProp(weights, biases, d.Xtrain_norm).run()
-
-# To calculate the cost of the network we take only the softmax predictions for
-# the class corresponding to the label, y, for each example.
-#
-# When using the network to make predictions we'll keep the predictions for all
-# classes and choose the one with the highest probability.
-predictions                      = np.choose(d.Ytrain, A[-1])
-c                                = Cost(predictions, d.Ytrain).cross_entropy_loss()
-weight_gradients, bias_gradients = BackwardProp(Z, A, d.Ytrain).run()
+weights, biases                  = Initialize(network_architecture).weights_and_biases()
+Z, A                             = ForwardProp(weights, biases, d.Xtrain_norm).run()
+c                                = Cost(A[-1], d.Ytrain).cross_entropy_loss()
+weight_gradients, bias_gradients = BackwardProp(weights, Z, A, d.Ytrain).run()
