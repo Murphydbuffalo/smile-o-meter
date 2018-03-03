0dd20292c43dd888fbeffb8c0525b0f5d541b594
Dan Murphy <murphydbuffalo@gmail.com>
2018-03-03 13:54:53 -0700

    Remove unnecessary thresholding of values.
    Originally I added code to ensure that values in forward prop and the
    cost function didn't overflow or underflow, but this turned out to be
    unnecessary. The reaason this numerical instability was occuring was
    because I wasn't computing the analytic gradients correctly during
    backprop. This resulted in gradients that were far too large, which in
    turn caused we network's weights to become too large, which finally
    resulted in super high values in forward prop and the cost function as
    more and more iterations were performed.

--- neural_net/cost.py
+++ neural_net/cost.py
@@ -6,10 +6,5 @@ class Cost:
         self.labels      = labels
         self.m           = float(labels.shape[1])
 
-    # To calculate the cost of the network we take only the softmax predictions
-    # for the class corresponding to the label of each example.
     def cross_entropy_loss(self):
-        near_zero                                      = 10 ** -300
-        self.predictions[self.predictions < near_zero] = near_zero
-
         return np.sum(-np.log(self.predictions) * self.labels) / self.m
