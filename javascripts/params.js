fb24fea67b9cf48f6546909dc68a0889af8fdee1
Dan Murphy <murphydbuffalo@gmail.com>
2018-03-31 17:24:44 -0600

    Properly load learned params JSON

--- javascripts/params.js
+++ javascripts/params.js
@@ -3,10 +3,12 @@
 const fs     = require('fs');
 const format = 'utf8';
 
+let weights, biases;
+
 try {
   // TODO: Host the JSON of the learned parameters in an S3 bucket and fetch() them
-  const weights = JSON.parse(fs.readFileSync('learned_params/learned_weights.json', format);
-  const biases  = JSON.parse(fs.readFileSync('learned_params/learned_biases.json',  format);
+  weights = JSON.parse(fs.readFileSync('../neural_net/weights.json', format));
+  biases  = JSON.parse(fs.readFileSync('../neural_net/biases.json',  format));
 } catch (e) {
   console.error('Error loading parameters:', e.message);
 }
--- javascripts/test.js
+++ javascripts/test.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const MockData            = require('./mock_data');
-const NeuralNet           = require('./neural_net');
+const mock                = new MockData();
 const { weights, biases } = require('./params');
+
+const NeuralNet           = require('./neural_net');
 const network             = new NeuralNet(mock.pixels(), weights, biases);
 
 network.predict();
