b0fbf5326333827cd5ea31c7d73d277a05b59d8f
Dan Murphy <murphydbuffalo@gmail.com>
2018-03-31 17:12:45 -0600

    Use learned params in test script

--- javascripts/test.js
+++ javascripts/test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const MockData  = require('./mock_data');
-const NeuralNet = require('./neural_net');
-const mock      = new MockData();
-const network   = new NeuralNet(mock.pixels(), mock.weights(), mock.biases());
+const MockData            = require('./mock_data');
+const NeuralNet           = require('./neural_net');
+const { weights, biases } = require('./params');
+const network             = new NeuralNet(mock.pixels(), weights, biases);
 
 network.predict();
