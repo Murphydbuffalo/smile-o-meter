69e7b5400aa3da2a554f36690926d741edb66c97
Dan Murphy <murphydbuffalo@gmail.com>
2018-04-14 17:48:11 -0600

    Rescale images to 48x48 and grayscale.
    So that we can feed the neural network the data format it expects.

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,16 +1,38 @@
 function changeExpression(mouth, eyes) {
-  return function(event) {
-    if (mouth.className.includes('frown-mouth')) {
-      mouth.className = 'mouth neutral-mouth';
-      eyes.forEach((eye) => eye.className = 'eye neutral-eyes');
-    } else if (mouth.className.includes('neutral-mouth')) {
-      mouth.className = 'mouth smile-mouth';
-      eyes.forEach((eye) => eye.className = 'eye smile-eyes');
-    } else {
-      mouth.className = 'mouth frown-mouth';
-      eyes.forEach((eye) => eye.className = 'eye frown-eyes');
+  return function(imageClass) {
+    switch (imageClass) {
+      case 0:
+        mouth.className = 'mouth neutral-mouth';
+        eyes.forEach((eye) => eye.className = 'eye neutral-eyes');
+        break;
+      case 1:
+        mouth.className = 'mouth smile-mouth';
+        eyes.forEach((eye) => eye.className = 'eye smile-eyes');
+        break;
+      case 2:
+        mouth.className = 'mouth frown-mouth';
+        eyes.forEach((eye) => eye.className = 'eye frown-eyes');
+        break;
     }
   }
+}
+
+// Takes an ImageData array, which contains four elements for each pixel (R, G, B, and A values)
+// Outputs an array one fourth the size, with the single greyscaled value for each pixel
+function grayscale(pixels) {
+  const grayPixels = [];
+  const length     = pixels.length
+  let i;
+
+  for (i = 0; i < length; i += 4) {
+    grayPixels.push((pixels[i] * 0.299) + (pixels[i + 1] * 0.587) + (pixels[i + 2] * 0.114));
+  }
+
+  return grayPixels;
+};
+
+function classify(grayscalePixels) {
+  return  Math.floor(Math.random() * 3);
 }
 
 window.addEventListener('load', function(event) {
@@ -20,18 +42,21 @@ window.addEventListener('load', function(event) {
   const video   = document.querySelector('video');
   const canvas  = document.querySelector('canvas');
   const context = canvas.getContext('2d');
+  context.scale(0.125, 0.125); // we want to feed a 48x48 image to the classifier
 
   navigator.mediaDevices
-           .getUserMedia({ video: true })
+           .getUserMedia({ video: { width: 384, height: 384 } })
            .then(function(mediaStream) {
              video.srcObject = mediaStream;
              video.play();
 
              setInterval(function() {
-               animate();
-               context.drawImage(video, 0, 0, 400, 300);
-               const pic = canvas.toDataURL();
-               console.log('pic is', pic.slice(0, 50) + '...');
+               context.drawImage(video, 0, 0, 384, 384);
+               const pixels          = context.getImageData(0, 0, 48, 48).data;
+               const grayscalePixels = grayscale(pixels);
+               const imageClass      = classify(grayscalePixels);
+
+               animate(imageClass);
              }, 1000);
            })
            .catch(function(err) {
