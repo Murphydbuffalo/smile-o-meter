abbab9492dfdc0bd3913801238a00bc932543c7d
Dan Murphy <murphydbuffalo@gmail.com>
2018-04-29 18:00:19 -0600

    Kill JS neural net & add a module to handle image data.
    Sending the learned parameters down to the client would've been
    prohibitively expensive: the weights are nearly 5MB before compression!
    
    Instead we're going to have to build a simple web service to run the
    Python neural net, and use that to send the predicted class down to the
    client.

---
+++ javascripts/image_manipulation.js
@@ -0,0 +1,81 @@
+const ImageManipulation = class ImageManipulation {
+  constructor(videoStream, videoDimension) {
+    this.videoStream                 = videoStream;
+    this.videoDimension              = videoDimension;
+    this.neuralNetworkImageDimension = 48;
+  }
+
+  renderGrayscale(grayscalePixels) {
+    const rgbaPixels = this.rgbaFromGrayscale(grayscalePixels);
+    const imageData  = this.context.createImageData(this.neuralNetworkImageDimension, this.neuralNetworkImageDimension);
+    imageData.data.set(rgbaPixels);
+    this.context.putImageData(imageData, 0, 0);
+  }
+
+  // Takes an ImageData array, which contains four elements for each pixel (R, G, B, and A values)
+  // Outputs an array one fourth the size, with the single greyscaled value for each pixel
+  grayscale(pixels) {
+    const grayPixels = [];
+    const length     = pixels.length
+    let i;
+
+    for (i = 0; i < length; i += 4) {
+      grayPixels.push((pixels[i] * 0.299) + (pixels[i + 1] * 0.587) + (pixels[i + 2] * 0.114));
+    }
+
+    return grayPixels;
+  };
+
+  rgbaFromGrayscale(pixels) {
+    const buffer = new Uint8ClampedArray(pixels.length * 4);
+    const length = pixels.length
+    let grayscaleIndex, bufferIndex;
+
+    for (grayscaleIndex = 0; grayscaleIndex < length; grayscaleIndex += 1) {
+      bufferIndex = grayscaleIndex * 4;
+      pixel       = pixels[grayscaleIndex];
+      buffer[bufferIndex]     = pixel // Red
+      buffer[bufferIndex + 1] = pixel // Green
+      buffer[bufferIndex + 2] = pixel // Blue
+      buffer[bufferIndex + 3] = 255;  // Alpha
+    }
+
+    return buffer;
+  }
+
+  start() {
+    this.video.srcObject = this.videoStream;
+    this.video.play();
+  }
+
+  get currentFrame() {
+    this.context.drawImage(this.video, 0, 0, this.videoDimension, this.videoDimension);
+    return this.context.getImageData(0, 0, this.neuralNetworkImageDimension, this.neuralNetworkImageDimension).data;
+  }
+
+  get video() {
+    return document.querySelector('video');
+  }
+
+  get canvas() {
+    return document.querySelector('canvas');
+  }
+
+  get context() {
+    const context = this.canvas.getContext('2d');
+    context.scale(this.scalingFactor, this.scalingFactor);
+
+    if (window.location.search.includes('show-grayscale')) {
+      this.canvas.className = 'show';
+    }
+
+    return context;
+  }
+
+  get scalingFactor() {
+    // we need to scale the images from the video feed to match the dimensions expected by the neural network
+    return this.neuralNetworkImageDimension / this.videoDimension;
+  }
+}
+
+export default ImageManipulation;
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,62 +1,49 @@
-function changeExpression(mouth, eyes) {
-  return function(imageClass) {
-    switch (imageClass) {
-      case 0:
-        mouth.className = 'mouth neutral-mouth';
-        eyes.forEach((eye) => eye.className = 'eye neutral-eyes');
-        break;
-      case 1:
-        mouth.className = 'mouth smile-mouth';
-        eyes.forEach((eye) => eye.className = 'eye smile-eyes');
-        break;
-      case 2:
-        mouth.className = 'mouth frown-mouth';
-        eyes.forEach((eye) => eye.className = 'eye frown-eyes');
-        break;
-    }
+import ImageManipulation from './image_manipulation.js';
+
+
+const animate = function(mouth, eyes, imageClass) {
+  switch (imageClass) {
+    case 0:
+      mouth.className = 'mouth neutral-mouth';
+      eyes.forEach((eye) => eye.className = 'eye neutral-eyes');
+      break;
+    case 1:
+      mouth.className = 'mouth smile-mouth';
+      eyes.forEach((eye) => eye.className = 'eye smile-eyes');
+      break;
+    case 2:
+      mouth.className = 'mouth frown-mouth';
+      eyes.forEach((eye) => eye.className = 'eye frown-eyes');
+      break;
   }
-}
-
-// Takes an ImageData array, which contains four elements for each pixel (R, G, B, and A values)
-// Outputs an array one fourth the size, with the single greyscaled value for each pixel
-function grayscale(pixels) {
-  const grayPixels = [];
-  const length     = pixels.length
-  let i;
-
-  for (i = 0; i < length; i += 4) {
-    grayPixels.push((pixels[i] * 0.299) + (pixels[i + 1] * 0.587) + (pixels[i + 2] * 0.114));
-  }
-
-  return grayPixels;
 };
 
-function classify(grayscalePixels) {
+const classify = function(grayscalePixels) {
+  // TODO: Call AWS Lambda with neural net code
   return  Math.floor(Math.random() * 3);
-}
+};
 
 window.addEventListener('load', function(event) {
-  const mouth   = document.querySelector('.mouth');
-  const eyes    = document.querySelectorAll('.eye');
-  const animate = changeExpression(mouth, eyes);
-  const video   = document.querySelector('video');
-  const canvas  = document.querySelector('canvas');
-  const context = canvas.getContext('2d');
-  context.scale(0.125, 0.125); // we want to feed a 48x48 image to the classifier
+  const mouth          = document.querySelector('.mouth');
+  const eyes           = document.querySelectorAll('.eye');
+  const videoDimension = 384.0;
 
   navigator.mediaDevices
-           .getUserMedia({ video: { width: 384, height: 384 } })
+           .getUserMedia({ video: { width: videoDimension, height: videoDimension } })
            .then(function(mediaStream) {
-             video.srcObject = mediaStream;
-             video.play();
+             const videoFeed = new ImageManipulation(mediaStream, videoDimension);
+             videoFeed.start();
 
              setInterval(function() {
-               context.drawImage(video, 0, 0, 384, 384);
-               const pixels          = context.getImageData(0, 0, 48, 48).data;
-               const grayscalePixels = grayscale(pixels);
+               const pixels          = videoFeed.currentFrame;
+               const grayscalePixels = videoFeed.grayscale(pixels);
                const imageClass      = classify(grayscalePixels);
 
-               animate(imageClass);
+               if (window.location.search.includes('show-grayscale')) {
+                 videoFeed.renderGrayscale(grayscalePixels);
+               }
+
+               animate(mouth, eyes, imageClass);
              }, 1000);
            })
            .catch(function(err) {
