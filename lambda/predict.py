c4632f47f3805f9d5c7e1140246fa685da5e363c
Dan Murphy <murphydbuffalo@gmail.com>
2018-05-05 16:43:52 -0600

    Define functions in order of invocation

--- lambda/predict.py
+++ lambda/predict.py
@@ -7,22 +7,7 @@ biases_file  = s3.download_file('smile-o-meter.rocks', 'learned_biases.npy',  'l
 weights      = np.load('learned_weights.npy')
 biases       = np.load('learned_biases.npy')
 
-def predict(event, context):
-    softmax_activation = forward_prop(event['pixels'])
-    return np.argmax(softmax_activation)
-
-def forward_prop(A):
-    for i in range(len(weights)):
-        Z = np.dot(weights[i], A) + biases[i]
-        A = relu(Z)
-
-    return softmax_activation(Z)
-
 def relu(Z):
-    # NOTE: np.maximum is NOT the same as np.max, which find the maximum value
-    # in the matrix (or in each row/column of the matrix).
-    # np.maximum maps the given array element-wise, returning the max of each
-    # element and the provided second arg (0 in this case)
     return np.maximum(Z, 0)
 
 def softmax(v):
@@ -31,3 +16,14 @@ def softmax(v):
 
 def softmax_activation(Z):
     return np.atleast_2d(np.apply_along_axis(softmax, 0, Z))
+
+def forward_prop(A):
+    for i in range(len(weights)):
+        Z = np.dot(weights[i], A) + biases[i]
+        A = relu(Z)
+
+    return softmax_activation(Z)
+
+def predict(event, context):
+    softmax_activation = forward_prop(event['pixels'])
+    return np.argmax(softmax_activation)
