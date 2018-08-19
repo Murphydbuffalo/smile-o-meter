37da31df3ca28c41bbef8d7a2cf5f1c7d45330bf
Dan Murphy <murphydbuffalo@gmail.com>
2018-08-19 14:16:08 -0600

    Tests for `Initialize`

---
+++ neural_net/tests/test_initialize.py
@@ -0,0 +1,35 @@
+import unittest
+
+from lib.initialize import Initialize
+
+# Run tests like so, probs want to wrap this in a script
+# https://stackoverflow.com/questions/1896918/running-unittest-with-typical-test-directory-structure#answer-24266885
+class TestInitialize(unittest.TestCase):
+    def setUp(self):
+        self.network_architecture = [2304, 250, 150, 7]
+        self.weights, self.biases = Initialize(self.network_architecture).weights_and_biases()
+
+    def test_weights_shape(self):
+        self.assertEqual(len(self.weights), 3)
+        self.assertEqual(self.weights[0].shape, (250, 2304))
+        self.assertEqual(self.weights[1].shape, (150, 250))
+        self.assertEqual(self.weights[2].shape, (7, 150))
+
+    def test_weights_magnitude(self):
+        self.assertTrue(abs(self.weights[0].mean()) < abs(self.weights[1].mean()))
+        self.assertTrue(abs(self.weights[1].mean()) < abs(self.weights[2].mean()))
+        self.assertTrue(abs(self.weights[2].mean()) < 0.5)
+
+    def test_biases_shape(self):
+        self.assertEqual(len(self.biases), 3)
+        self.assertEqual(self.biases[0].shape, (250, 1))
+        self.assertEqual(self.biases[1].shape, (150, 1))
+        self.assertEqual(self.biases[2].shape, (7, 1))
+
+    def test_biases_magnitude(self):
+        self.assertTrue((self.biases[0] == 0).all())
+        self.assertTrue((self.biases[1] == 0).all())
+        self.assertTrue((self.biases[2] == 0).all())
+
+if __name__ == '__main__':
+    unittest.main()
