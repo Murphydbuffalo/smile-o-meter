ea4f6a6f14b539245eed8600d444a5eb21aa4b64
Dan Murphy <murphydbuffalo@gmail.com>
2018-08-26 11:38:31 -0600

    Don't hardcode # of layers in RMSProp denominator calculation

--- neural_net/lib/optimizers/adam.py
+++ neural_net/lib/optimizers/adam.py
@@ -15,37 +15,33 @@ class Adam:
         self.rms_prop_bias_average   = np.zeros(biases.shape)
 
     def update_parameters(self, weight_gradients, bias_gradients):
-        self.momentum_weight_average = self.__updated_momentum_weight_average(weight_gradients)
-        self.momentum_bias_average   = self.__updated_momentum_bias_average(bias_gradients)
-        self.rms_prop_weight_average = self.__updated_rms_prop_weight_average(weight_gradients)
-        self.rms_prop_bias_average   = self.__updated_rms_prop_bias_average(bias_gradients)
+        self.momentum_weight_average = self.updated_momentum_weight_average(weight_gradients)
+        self.momentum_bias_average   = self.updated_momentum_bias_average(bias_gradients)
+        self.rms_prop_weight_average = self.updated_rms_prop_weight_average(weight_gradients)
+        self.rms_prop_bias_average   = self.updated_rms_prop_bias_average(bias_gradients)
 
-        self.weights = self.weights - (self.learning_rate * self.momentum_weight_average / self.__weight_denominator())
-        self.biases  = self.biases  - (self.learning_rate * self.momentum_bias_average   / self.__bias_denominator())
+        self.weights = self.weights - (self.learning_rate * self.momentum_weight_average / self.weight_denominator())
+        self.biases  = self.biases  - (self.learning_rate * self.momentum_bias_average   / self.bias_denominator())
 
-    def __updated_momentum_weight_average(self, weight_gradients):
+    def updated_momentum_weight_average(self, weight_gradients):
         return (self.momentum_rate * self.momentum_weight_average) + ((1 - self.momentum_rate) * weight_gradients)
 
-    def __updated_momentum_bias_average(self, bias_gradients):
+    def updated_momentum_bias_average(self, bias_gradients):
         return (self.momentum_rate * self.momentum_bias_average) + ((1 - self.momentum_rate) * bias_gradients)
 
-    def __updated_rms_prop_weight_average(self, weight_gradients):
+    def updated_rms_prop_weight_average(self, weight_gradients):
         return (self.rms_prop_rate * self.rms_prop_weight_average) + ((1 - self.rms_prop_rate) * np.square(weight_gradients))
 
-    def __updated_rms_prop_bias_average(self, bias_gradients):
+    def updated_rms_prop_bias_average(self, bias_gradients):
         return (self.rms_prop_rate * self.rms_prop_bias_average) + ((1 - self.rms_prop_rate) * np.square(bias_gradients))
 
-    def __weight_denominator(self):
-        # Why can't we call np.sqrt on an n-dimensional array?
-        return np.array([
-            np.sqrt(self.rms_prop_weight_average[0]),
-            np.sqrt(self.rms_prop_weight_average[1]),
-            np.sqrt(self.rms_prop_weight_average[2])
-        ]) + self.epsilon
+    def weight_denominator(self):
+        square_roots = self.square_roots(self.rms_prop_weight_average)
+        return np.array(square_roots) + self.epsilon
 
-    def __bias_denominator(self):
-        return np.array([
-            np.sqrt(self.rms_prop_bias_average[0]),
-            np.sqrt(self.rms_prop_bias_average[1]),
-            np.sqrt(self.rms_prop_bias_average[2])
-        ]) + self.epsilon
+    def bias_denominator(self):
+        square_roots = self.square_roots(self.rms_prop_bias_average)
+        return np.array(square_roots) + self.epsilon
+
+    def square_roots(self, n_dimensional_array):
+        return list(np.sqrt(layer) for layer in n_dimensional_array)
