9278f4fb3f17f4e90fa44045d8333320fc0110b0
Dan Murphy <murphydbuffalo@gmail.com>
2018-08-26 12:14:47 -0600

    Tests for `GradientDescent`

---
+++ neural_net/tests/optimizers/test_gradient_descent.py
@@ -0,0 +1,43 @@
+import unittest
+import numpy as np
+
+from lib.optimizers.gradient_descent import GradientDescent
+
+num_input_features     = 10
+num_hidden_layer_nodes = 5
+num_classes            = 3
+
+class TestGradientDescent(unittest.TestCase):
+    def setUp(self):
+        self.weights = np.array([
+            np.random.randn(num_hidden_layer_nodes, num_input_features),
+            np.random.randn(num_classes, num_hidden_layer_nodes),
+        ])
+
+        self.biases = np.array([
+            np.zeros((num_hidden_layer_nodes, 1)),
+            np.zeros((num_classes, 1)),
+        ])
+
+        self.gradient_descent = GradientDescent(self.weights, self.biases)
+
+    def test_update_parameters(self):
+        weight_gradients = np.array([
+            np.ones((num_hidden_layer_nodes, num_input_features)),
+            np.ones((num_classes, num_hidden_layer_nodes)),
+        ])
+
+        bias_gradients = np.array([
+            np.random.randn(num_hidden_layer_nodes, 1),
+            np.random.randn(num_classes, 1),
+        ])
+
+        self.gradient_descent.update_parameters(weight_gradients, bias_gradients)
+
+        expected_updated_weights = self.weights - GradientDescent.learning_rate
+
+        self.assertTrue(np.isclose(self.gradient_descent.weights[0], expected_updated_weights[0]).all())
+        self.assertTrue(np.isclose(self.gradient_descent.weights[1], expected_updated_weights[1]).all())
+
+if __name__ == '__main__':
+    unittest.main()
