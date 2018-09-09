66aaecf75e693d45dd1e2df249d14c73c435dab9
Dan Murphy <murphydbuffalo@gmail.com>
2018-09-09 12:05:34 -0600

    Clean up Adam test

--- neural_net/tests/optimizers/test_adam.py
+++ neural_net/tests/optimizers/test_adam.py
@@ -3,26 +3,26 @@ import numpy as np
 
 from lib.optimizers.adam import Adam
 
-num_input_features     = 10
-num_hidden_layer_nodes = 5
-num_classes            = 3
-
+# Adam is a "momentum" based optimizer. It's similar to GradientDescent in
+# that it updates parameters by subtracting the gradients (derivatives) of
+# those parameters multiplied by some small coefficient (the learning rate).
+# Let's call that product the "update amount".
+#
+# Adam differs from GradientDescent in that its update amount also includes
+# a weighted average of previous update amounts. So, as you iteratively call
+# `Adam#update_parameters(weight_gradients, bias_gradients)`, the update
+# amount is influenced more and more by its previous values, and less by the
+# gradients passed in to that iteration. In this way Adam "builds momentum"
+# by updating parameters in a way that over time becomes less responsive to
+# the arguments passed in, and more similar to previous updates. This method
+# is far more effective at quickly finding optimal paramters than non-momentum
+# based algorithms like GradientDescent.
 class TestAdam(unittest.TestCase):
-    # Adam is a "momentum" based optimizer. It's similar to GradientDescent in
-    # that it updates parameters by subtracting the gradients (derivatives) of
-    # those parameters multiplied by some small coefficient (the learning rate).
-    # Let's call that product the "update amount".
-    #
-    # Adam differs from GradientDescent in that its update amount also includes
-    # a weighted average of previous update amounts. So, as you iteratively call
-    # `Adam#update_parameters(weight_gradients, bias_gradients)`, the update
-    # amount is influenced more and more by its previous values, and less by the
-    # gradients passed in to that iteration. In this way Adam "builds momentum"
-    # by updating parameters in a way that over time becomes less responsive to
-    # the arguments passed in, and more similar to previous updates. This method
-    # is far more effective at quickly finding optimal paramters than non-momentum
-    # based algorithms like GradientDescent.
-    def test_parameters_decrease_at_a_decreasing_rate(self):
+    def setUp(self):
+        num_input_features     = 10
+        num_hidden_layer_nodes = 5
+        num_classes            = 3
+
         weights = np.array([
             np.ones((num_hidden_layer_nodes, num_input_features)) * 0.01,
             np.ones((num_classes, num_hidden_layer_nodes))        * 0.01
@@ -33,25 +33,32 @@ class TestAdam(unittest.TestCase):
             np.zeros((num_classes, 1))
         ])
 
-        weight_gradients = np.copy(weights)
+        self.weight_gradients = np.copy(weights)
 
-        bias_gradients = np.array([
+        self.bias_gradients = np.array([
             np.random.randn(num_hidden_layer_nodes, 1),
             np.random.randn(num_classes, 1)
         ])
 
-        adam = Adam(weights, biases)
+        self.adam = Adam(weights, biases)
 
+    def test_parameters_decrease_at_a_decreasing_rate(self):
         update_amounts = []
+
         for i in range(0, 10):
-            weights_before_update = adam.weights
-            adam.update_parameters(weight_gradients, bias_gradients)
-            update_amounts.append(abs(weights_before_update[0] - adam.weights[0]).mean())
+            weights_before_update = self.adam.weights
+            self.adam.update_parameters(self.weight_gradients, self.bias_gradients)
+
+            update_amount = abs(weights_before_update[0] - self.adam.weights[0]).mean()
+            update_amounts.append(update_amount)
 
         update_amount_deltas = []
+
         for i in range(0, len(update_amounts) - 1):
             self.assertTrue(update_amounts[i] < update_amounts[i + 1])
-            update_amount_deltas.append(abs(update_amounts[i] - update_amounts[i + 1]))
+
+            update_amount_delta = abs(update_amounts[i] - update_amounts[i + 1])
+            update_amount_deltas.append(update_amount_delta)
 
         for i in range(0, len(update_amount_deltas) - 1):
             self.assertTrue(update_amount_deltas[i] > update_amount_deltas[i + 1])
