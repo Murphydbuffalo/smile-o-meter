cad89266afe0945e5576654f9204bf7eded07252
Dan Murphy <murphydbuffalo@gmail.com>
2018-09-29 10:44:52 -0600

    Use `keepdims` to avoid manual array construction

--- neural_net/lib/data/normalizer.py
+++ neural_net/lib/data/normalizer.py
@@ -7,17 +7,17 @@ class Normalizer:
         self.test_examples       = test_examples
 
     def normalize(self):
-        training_set_means                         = np.array([np.mean(self.training_examples, 1)]).T
+        training_set_means                         = self.training_examples.mean(axis = 1, keepdims = True)
         zero_mean_training_data                    = self.training_examples - training_set_means
-        zero_mean_training_set_standard_deviations = np.array([np.std(zero_mean_training_data, 1)]).T
+        zero_mean_training_set_standard_deviations = zero_mean_training_data.std(axis = 1, keepdims = True)
 
         # Transform training data so it has mean 0 and variance 1
-        self.normalized_training_examples   = (zero_mean_training_data) / zero_mean_training_set_standard_deviations
+        self.normalized_training_examples   = zero_mean_training_data / zero_mean_training_set_standard_deviations
 
         # Perform analagous transformation on the validation and test data
-        adjusted_mean_validation_data       = self.validation_examples  - training_set_means
-        adjusted_mean_test_data             = self.test_examples        - training_set_means
-        self.normalized_validation_examples = (adjusted_mean_validation_data) / zero_mean_training_set_standard_deviations
-        self.normalized_test_examples       = (adjusted_mean_test_data)       / zero_mean_training_set_standard_deviations
+        adjusted_mean_validation_data       = self.validation_examples - training_set_means
+        adjusted_mean_test_data             = self.test_examples       - training_set_means
+        self.normalized_validation_examples = adjusted_mean_validation_data / zero_mean_training_set_standard_deviations
+        self.normalized_test_examples       = adjusted_mean_test_data       / zero_mean_training_set_standard_deviations
 
         return self
