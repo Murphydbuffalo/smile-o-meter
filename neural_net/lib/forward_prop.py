74d275e1887b434859256a3c3a65fe4cd80d7d1e
Dan Murphy <murphydbuffalo@gmail.com>
2018-11-11 10:48:45 -0700

    Get rid of double underscores

--- neural_net/lib/forward_prop.py
+++ neural_net/lib/forward_prop.py
@@ -11,17 +11,17 @@ class ForwardProp:
     def run(self):
         for i in range(len(self.weights)):
             linear_activation    = np.dot(self.weights[i], self.nonlinear_activations[i])  + self.biases[i]
-            nonlinear_activation = self.__relu(linear_activation)
+            nonlinear_activation = self.relu(linear_activation)
 
             self.linear_activations.append(linear_activation)
             self.nonlinear_activations.append(nonlinear_activation)
 
-        self.network_output            = self.__softmax_activation(linear_activation)
+        self.network_output            = self.softmax_activation(linear_activation)
         self.nonlinear_activations[-1] = self.network_output
 
         return [self.linear_activations, self.nonlinear_activations]
 
-    def __relu(self, linear_activation):
+    def relu(self, linear_activation):
         # NOTE: np.maximum is NOT the same as np.max
         # np.max finds the maximum value in the matrix (or in each row/column
         # of the matrix)
@@ -30,9 +30,9 @@ class ForwardProp:
         # each element and the provided second arg (0 in this case)
         return np.maximum(linear_activation, 0)
 
-    def __softmax(self, vector):
+    def softmax(self, vector):
         exponentials = np.exp(vector - np.max(vector, axis = 0))
         return exponentials / np.sum(exponentials)
 
-    def __softmax_activation(self, linear_activation):
-        return np.atleast_2d(np.apply_along_axis(self.__softmax, 0, linear_activation))
+    def softmax_activation(self, linear_activation):
+        return np.atleast_2d(np.apply_along_axis(self.softmax, 0, linear_activation))
